737f59d164aecc5c234839131873495a60c42ec4
Kirkleve <102994769+Kirkleve@users.noreply.github.com>
2022-04-17 23:31:15 +0200

    Создал функции, но не проходит победа в рулетке

--- Element4/Program.cs
+++ Element4/Program.cs
@@ -3,20 +3,27 @@
 //Доп. задача с усложнением: на отгадывание дается 3 попытки.
 //По возможности использовать функции.
 
-int rand = new Random ().Next(1,1000);
+                            //Здесь тренировался с функциями Element4pro обычное решение
+
+int rand = RandomInt(1,100);
+int RandomInt(int min, int max)
+{
+    return new Random ().Next(min, max);
+}
 Console.WriteLine(rand);
 Console.WriteLine("Отгадай число: ");
 int mystery = Convert.ToInt32(Console.ReadLine());
 int index = 0;
 
-while (index < 2)
+void Print (int arg)
 {
-    if (mystery == rand)
-    {
-        Console.WriteLine("Мои поздрвавления вы угадали!))");
-        break;
-    }    
-    if (rand > mystery) 
+    Console.WriteLine(arg);
+}
+
+int last = FestChanse(rand, mystery);
+int FestChanse(int ran, int mys)
+{
+    if (ran > mys) 
     {
         Console.WriteLine("Загаданное число больше, попробуй снова");
         index++;
@@ -26,9 +33,14 @@ while (index < 2)
         Console.WriteLine("Загаданное число меньше, ещё есть попытка");
         index++;
     }     
-    int last = Convert.ToInt32(Console.ReadLine());  
-    
-    if (rand > last) 
+    int last = Convert.ToInt32(Console.ReadLine());
+    return last;
+}
+
+int last2 = SecondChanse(rand, last);
+int SecondChanse(int ran, int mys)
+{
+    if (ran > mys) 
     {
         Console.WriteLine("Загаданное число больше, попробуй снова");
         index++;
@@ -39,16 +51,35 @@ while (index < 2)
         index++;
     }     
     int last2 = Convert.ToInt32(Console.ReadLine());
-    
-    if (rand > last2) 
+    return last2;
+}
+
+int lastchense = LastChanse(rand, last2);
+int LastChanse(int ran,int mys)
+{
+    if (ran > mys) 
     {
         Console.WriteLine("Загаданное число больше, увы ты проиграл");
-        break;
+        return ran;
     }
     else 
     {
         Console.WriteLine("Загаданное число меньше, увы ты проиграл");
-        break;
+        return mys;
     }     
 }
-Console.WriteLine("игра закончина");
+
+while (index < 2)
+{
+    if (rand == mystery)
+    {
+        Console.WriteLine("Мои поздрвавления вы угадали!");
+    }
+    else
+    {
+    Print(last);
+    Print(last2);
+    Print(lastchense);
+    }
+}
+
---
+++ Element4pro/Program.cs
@@ -0,0 +1,55 @@
+﻿int rand = RandomInt(1,100);
+int RandomInt(int min, int max)
+{
+    return new Random ().Next(min, max);
+}
+Console.WriteLine(rand);
+Console.WriteLine("Отгадай число: ");
+int mystery = Convert.ToInt32(Console.ReadLine());
+
+int index = 0;
+
+
+while (index < 2)
+{
+    if (mystery == rand)
+    {
+        Console.WriteLine("Мои поздрвавления вы угадали!))");
+        break;
+    }    
+    if (rand > mystery) 
+    {
+        Console.WriteLine("Загаданное число больше, попробуй снова");
+        index++;
+    }
+    else 
+    {
+        Console.WriteLine("Загаданное число меньше, ещё есть попытка");
+        index++;
+    }     
+    int last = Convert.ToInt32(Console.ReadLine());  
+    
+    if (rand > last) 
+    {
+        Console.WriteLine("Загаданное число больше, попробуй снова");
+        index++;
+    }
+    else 
+    {
+        Console.WriteLine("Загаданное число меньше, ещё есть попытка");
+        index++;
+    }     
+    int last2 = Convert.ToInt32(Console.ReadLine());
+    
+    if (rand > last2) 
+    {
+        Console.WriteLine("Загаданное число больше, увы ты проиграл");
+        break;
+    }
+    else 
+    {
+        Console.WriteLine("Загаданное число меньше, увы ты проиграл");
+        break;
+    }     
+}
+Console.WriteLine("игра закончина");
